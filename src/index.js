03f0a11275c064cc8ad171cd24081d4b8b37524f
emilie-coder <emilie.n.hopkinson.22@dartmouth.edu>
2022-04-15 19:57:54 -0400

    starter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-// change require to es6 import style
-import $ from 'jquery';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import './style.scss';
 
-let currTime = 0;
-
-function incrementTime() {
-  currTime += 1;
-  $('#main').html(`youve been on this pagee for ${currTime} seconds`);
+function App() {
+  return <div className="test">All the REACT are belong to us!</div>;
 }
 
-const overall = setInterval(incrementTime, 1000);
-overall();
+const root = createRoot(document.getElementById('main'));
+root.render(<App />);
