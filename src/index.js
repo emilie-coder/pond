60c7f0fc954c5ca49c10c9ffcb15f7af12300e7e
emilie-coder <emilie.n.hopkinson.22@dartmouth.edu>
2022-04-15 00:42:14 -0400

    final commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('starting up!');
+// change require to es6 import style
+import $ from 'jquery';
+import './style.scss';
+
+let currTime = 0;
+
+function incrementTime() {
+  currTime += 1;
+  $('#main').html(`youve been on this pagee for ${currTime} seconds`);
+}
+
+const overall = setInterval(incrementTime, 1000);
+overall();
