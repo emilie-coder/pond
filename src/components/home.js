40ceb99c3ff282645864d0d02f39f4f1fc2b0276
emilie-coder <emilie.n.hopkinson.22@dartmouth.edu>
2022-07-07 13:47:13 -0700

    starting text

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,27 @@
 import React from 'react';
 
 function Home(props) {
-  return <div> home page </div>;
+  return (
+    <div className="hp_main">
+      <div className="hp_sub">
+        <h2 className="hp_subTitle"> Innovative Solutions</h2>
+        <h3 className="hp_subText"> for new and upcoming artists</h3>
+      </div>
+      <div className="hp_sub">
+        <h2 className="hp_subTitle"> We Help musicians</h2>
+      </div>
+      <div className="hp_sub">
+        <h2 className="hp_subTitle"> Do what they love</h2>
+        <h3 className="hp_subText"> allowing them to continue their craft</h3>
+        <h3 className="hp_subText"> while we</h3>
+      </div>
+      <div className="hp_sub">
+        <h2 className="hp_subTitle"> How we do it</h2>
+      </div>
+      <div className="hp_sub">
+        <h2 className="hp_subTitle"> Sign up for more information </h2>
+      </div>
+    </div>
+  );
 }
 export default Home;
